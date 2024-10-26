49f447b415c6214121e2a698c743d39db3b4daa6
Otto@AirM2 <cia1099@gmail.com>
2024-10-26 13:13:50 +0800

    completed customized bottom app bar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,96 +9,112 @@ import '../views/chart_tab.dart';
 import '../views/setting_tab.dart';
 import '../widgets/definition_tile.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var _index = 0;
+  final bottomItems = [
+    BottomNavigationBarItem(
+        icon: PlatformWidget(
+          material: (_, __) => Icon(Icons.book_outlined),
+          cupertino: (_, __) => Icon(CupertinoIcons.book),
+        ),
+        label: "vocabulary"),
+    BottomNavigationBarItem(
+        icon: PlatformWidget(
+          material: (_, __) => Icon(Icons.chat_bubble_outline),
+          cupertino: (_, __) => Icon(CupertinoIcons.chat_bubble_2),
+        ),
+        label: "chat"),
+    BottomNavigationBarItem(
+        icon: PlatformWidget(
+          material: (_, __) => Icon(Icons.bar_chart),
+          cupertino: (_, __) => Icon(CupertinoIcons.doc_chart),
+        ),
+        label: "charts"),
+    BottomNavigationBarItem(
+        icon: PlatformWidget(
+          material: (_, __) => Icon(Icons.settings),
+          cupertino: (_, __) => Icon(CupertinoIcons.gear),
+        ),
+        label: "setting"),
+  ];
+  @override
   Widget build(BuildContext context) {
     // return simpleScaffold(context);
+    // return tabScaffold(bottomItems, context);
 
-    final bottomItems = [
-      BottomNavigationBarItem(
-          icon: PlatformWidget(
-            material: (_, __) => Icon(Icons.book_outlined),
-            cupertino: (_, __) => Icon(CupertinoIcons.book),
-          ),
-          label: "vocabulary"),
-      BottomNavigationBarItem(
-          icon: PlatformWidget(
-            material: (_, __) => Icon(Icons.chat_bubble_outline),
-            cupertino: (_, __) => Icon(CupertinoIcons.chat_bubble_2),
-          ),
-          label: "chat"),
-      BottomNavigationBarItem(
-          icon: PlatformWidget(
-            material: (_, __) => Icon(Icons.bar_chart),
-            cupertino: (_, __) => Icon(CupertinoIcons.doc_chart),
-          ),
-          label: "charts"),
-      BottomNavigationBarItem(
-          icon: PlatformWidget(
-            material: (_, __) => Icon(Icons.settings),
-            cupertino: (_, __) => Icon(CupertinoIcons.gear),
-          ),
-          label: "setting"),
-    ];
     return Scaffold(
       appBar: PreferredSize(
           preferredSize: Size.fromHeight(kToolbarHeight),
-          child: PlatformAppBar()),
-      body: DefaultTabController(
-        length: bottomItems.length,
-        child: TabBarView(
-            physics: NeverScrollableScrollPhysics(),
-            children: List.generate(
-              4,
-              (index) => IndexedStack(index: index, children: [
-                Center(child: Text("Page ${index + 1}")),
-                AlphabetListTab(
-                    contacts: List.generate(512,
-                        (i) => ClientModel(name: createName(), userId: i + 1))),
-                ChartTab(),
-                SettingTab(),
-              ]),
-            )),
-      ),
+          child: PlatformAppBar(
+            title: Text('${bottomItems[_index].label}'),
+            material: (_, __) => MaterialAppBarData(
+              backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+            ),
+          )),
+      body: IndexedStack(index: _index, children: [
+        Center(child: Text("Page ${_index + 1}")),
+        AlphabetListTab(
+            contacts: List.generate(
+                512, (i) => ClientModel(name: createName(), userId: i + 1))),
+        ChartTab(),
+        SettingTab(),
+      ]),
       bottomNavigationBar: BottomAppBar(
-        height: kBottomNavigationBarHeight + 28,
+        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+        height: kBottomNavigationBarHeight,
         // color: Colors.red,
+        notchMargin: 8,
         shape: CircularNotchedRectangle(),
-        child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: List.generate(bottomItems.length + 1, (i) {
-              var index = i;
-              if (i == 2)
-                return SizedBox(width: 28);
-              else if (i > 2) {
-                index -= 1;
-              }
-              return IconButton(
-                  onPressed: () {
-                    final tabController = DefaultTabController.of(context);
-                    setState(() {
-                      tabController.index = index;
-                    });
-                  },
-                  tooltip: bottomItems[index].tooltip,
-                  icon: Column(
-                    children: [
-                      bottomItems[index].icon,
-                      Text('${bottomItems[index].label}')
-                    ],
-                  ));
-            })),
+        child: MediaQuery.removePadding(
+          context: context,
+          removeTop: true,
+          child: Row(
+              // crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: List.generate(bottomItems.length + 1, (i) {
+                var index = i;
+                if (i == 2)
+                  return SizedBox(width: 32);
+                else if (i > 2) {
+                  index -= 1;
+                }
+
+                return IconButton(
+                    padding: EdgeInsets.zero,
+                    onPressed: () => _index == index
+                        ? null
+                        : setState(() {
+                            _index = index;
+                          }),
+                    tooltip: bottomItems[index].tooltip,
+                    icon: Column(
+                      children: [
+                        bottomItems[index].icon,
+                        Text('${bottomItems[index].label}')
+                      ],
+                    ));
+              })),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         child: Icon(CupertinoIcons.add),
         shape: CircleBorder(),
-        elevation: 0,
+        elevation: 1,
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
+  }
+
+  PlatformTabScaffold tabScaffold(
+      List<BottomNavigationBarItem> bottomItems, BuildContext context) {
     return PlatformTabScaffold(
       tabController: PlatformTabController(),
       iosContentPadding: true,
@@ -108,14 +124,30 @@ class HomePage extends StatelessWidget {
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         ),
       ),
-      bodyBuilder: (_, index) => IndexedStack(index: index, children: [
-        Center(child: Text("Page ${index + 1}")),
-        AlphabetListTab(
-            contacts: List.generate(
-                512, (i) => ClientModel(name: createName(), userId: i + 1))),
-        ChartTab(),
-        SettingTab(),
-      ]),
+      bodyBuilder: (_, index) => Stack(
+        children: [
+          IndexedStack(index: index, children: [
+            Center(child: Text("Page ${index + 1}")),
+            AlphabetListTab(
+                contacts: List.generate(512,
+                    (i) => ClientModel(name: createName(), userId: i + 1))),
+            ChartTab(),
+            SettingTab(),
+          ]),
+          Align(
+            alignment: FractionalOffset(.5, .9),
+            child: Offstage(
+              offstage: platform(context) != PlatformTarget.iOS,
+              child: FloatingActionButton(
+                onPressed: () {},
+                tooltip: 'My note',
+                shape: CircleBorder(),
+                child: const Icon(CupertinoIcons.add),
+              ),
+            ),
+          )
+        ],
+      ),
       items: bottomItems,
       cupertino: (_, __) => CupertinoTabScaffoldData(),
       cupertinoTabs: (_, __) => CupertinoTabBarData(),
