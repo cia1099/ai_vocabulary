85a358f4c06769fd9602a589614dc6d30e4cf392
Otto@AirM2 <cia1099@gmail.com>
2024-11-25 14:01:27 +0800

    reorganized word provider code

---
+++ lib/provider/public_word_provider.dart
@@ -0,0 +1,66 @@
+part of 'word_provider.dart';
+
+extension PublicWordProvider on WordProvider {
+  void nextStudyWord([int? index]) {
+    if (index == null) {
+      _learnedIndex = min(_learnedIndex + 1, _studyWords.length);
+    }
+    final word = _studyWords.elementAtOrNull(index ?? _learnedIndex);
+    _currentWord = word;
+    _providerState.add(word);
+    if (word != null) {
+      _remindIDs.add(word.wordId);
+    }
+    if (_studyWords.isNotEmpty) {
+      if (_learnedIndex == _studyWords.length) {
+        Future.delayed(Durations.extralong4, () => _subscript.pause());
+      } else {
+        _subscript.resume();
+      }
+    }
+  }
+
+  bool shouldRemind() {
+    return _remindIDs.isNotEmpty && _remindIDs.length % 7 == 0 ||
+        _learnedIndex == _studyWords.length - 1 ||
+        _learnedIndex == reviewCount - 1;
+  }
+
+  List<Vocabulary> remindWords() {
+    final reminds =
+        _studyWords.where((w) => _remindIDs.contains(w.wordId)).toList();
+    _remindIDs.clear();
+    return reminds;
+  }
+
+  bool shouldResample(final File file) {
+    if (!file.existsSync()) return true;
+    final obj = json.decode(file.readAsStringSync());
+    final now = DateTime.now().millisecondsSinceEpoch ~/ 1000;
+    return now - obj['dateTime'] >= 86400;
+  }
+
+  Future<List<Vocabulary>> requestWords(Set<int> wordIds) async {
+    var words = <Vocabulary>[];
+    Exception? error = ApiException('initial');
+    while (error != null) {
+      try {
+        words = await getWords(wordIds);
+        error = null;
+      } on ApiException catch (e) {
+        final errorIds = splitWords(e.message).expand((w) sync* {
+          if (w.contains(RegExp(r'^-?\d+$'))) yield w;
+        }).map((s) => int.parse(s));
+        print(errorIds);
+        wordIds.removeAll(errorIds);
+        wordIds = await _sampleWordIds(wordIds, studyCount);
+        error = e;
+      }
+    }
+    return words;
+  }
+
+  List<Vocabulary> subList([int start = 0, int? end]) {
+    return _studyWords.sublist(start, end);
+  }
+}
--- lib/provider/word_provider.dart
+++ lib/provider/word_provider.dart
@@ -10,7 +10,8 @@ import 'package:path/path.dart' as p;
 import 'package:ai_vocabulary/api/dict_api.dart';
 import 'package:ai_vocabulary/model/vocabulary.dart';
 import 'package:ai_vocabulary/utils/regex.dart';
-// import 'package:path_provider/path_provider.dart';
+
+part 'public_word_provider.dart';
 
 class WordProvider {
   final _studyWords = <Vocabulary>[];
@@ -100,38 +101,6 @@ class WordProvider {
     _providerState.close();
   }
 
-  void nextStudyWord([int? index]) {
-    if (index == null) {
-      _learnedIndex = min(_learnedIndex + 1, _studyWords.length);
-    }
-    final word = _studyWords.elementAtOrNull(index ?? _learnedIndex);
-    _currentWord = word;
-    _providerState.add(word);
-    if (word != null) {
-      _remindIDs.add(word.wordId);
-    }
-    if (_studyWords.isNotEmpty) {
-      if (_learnedIndex == _studyWords.length) {
-        Future.delayed(Durations.extralong4, () => _subscript.pause());
-      } else {
-        _subscript.resume();
-      }
-    }
-  }
-
-  bool shouldRemind() {
-    return _remindIDs.isNotEmpty && _remindIDs.length % 7 == 0 ||
-        _learnedIndex == _studyWords.length - 1 ||
-        _learnedIndex == reviewCount - 1;
-  }
-
-  List<Vocabulary> remindWords() {
-    final reminds =
-        _studyWords.where((w) => _remindIDs.contains(w.wordId)).toList();
-    _remindIDs.clear();
-    return reminds;
-  }
-
   Future<Set<int>> _sampleWordIds(
       Iterable<int> reviewIDs, final int count) async {
     final maxId = await getMaxId();
@@ -161,37 +130,6 @@ class WordProvider {
       }
       while (_studyWords[j].wordId == wordIds[j] && j > i) j--;
     }
-  }
-
-  bool shouldResample(final File file) {
-    if (!file.existsSync()) return true;
-    final obj = json.decode(file.readAsStringSync());
-    final now = DateTime.now().millisecondsSinceEpoch ~/ 1000;
-    return now - obj['dateTime'] >= 86400;
-  }
-
-  Future<List<Vocabulary>> requestWords(Set<int> wordIds) async {
-    var words = <Vocabulary>[];
-    Exception? error = ApiException('initial');
-    while (error != null) {
-      try {
-        words = await getWords(wordIds);
-        error = null;
-      } on ApiException catch (e) {
-        final errorIds = splitWords(e.message).expand((w) sync* {
-          if (w.contains(RegExp(r'^-?\d+$'))) yield w;
-        }).map((s) => int.parse(s));
-        print(errorIds);
-        wordIds.removeAll(errorIds);
-        wordIds = await _sampleWordIds(wordIds, studyCount);
-        error = e;
-      }
-    }
-    return words;
-  }
-
-  List<Vocabulary> subList([int start = 0, int? end]) {
-    return _studyWords.sublist(start, end);
   }
 }
 
